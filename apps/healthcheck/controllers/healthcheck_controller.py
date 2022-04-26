0f19af895e684c00d2300196250de11131f99b98
johnny evans <evanseburu32@gmail.com>
2022-04-26 22:53:48 +0100

    lets gooo

--- apps/healthcheck/controllers/healthcheck_controller.py
+++ apps/healthcheck/controllers/healthcheck_controller.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from fastapi import Response, status
 from fastapi.routing import APIRouter
 
 from apps.appclient.services.appclient_service import AppClientService
+from core.utils.response_service import ResponseService
 
 router = APIRouter(prefix="/api/v1/health-check", tags=["Health Check 🩺"])
 
 
 appClientService = AppClientService()
+responseService = ResponseService()
 
 client_auth = appClientService.client_auth
 
 
 @router.get("")
-def health_check():
-    return "all good here, workkkks"
+def health_check(res: Response):
+    return responseService.send_response(
+        res, status.HTTP_200_OK, "all good here, works"
+    )
