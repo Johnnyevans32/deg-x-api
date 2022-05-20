ada31d17567a5df41524e1454d396462aec43f89
johnny evans <evanseburu32@gmail.com>
2022-05-20 21:25:48 +0100

    circle ci

--- noxfile.py
+++ noxfile.py
@@ -1,30 +1,57 @@
 import tempfile
+
 import nox
 
 
 @nox.session(python=["3.10"])
-def tests(session: nox.Session):
+def shared_venv(session):
+    session.install("poetry")
     session.run("poetry", "install")
     session.install("-r", "requirements.txt")
-    session.run("python", "-m", "coverage", "run", "-m", "pytest")
-    session.run("coverage", "report")
+
+    if session.posargs:
+        match session.posargs[0]:
+            case 'tests':
+                session.run("python", "-m", "coverage", "run", "-m", "pytest")
+                session.run("coverage", "report")
+            case 'lint':
+                session.run("black", "--check", ".", "--target-version", "py310")
+                session.run("flake8", ".")
+            case 'typing':
+                session.run("mypy", "--config-file", "mypy.ini", ".")
+            case _:
+                pass
 
 
 @nox.session
-def lint(session: nox.Session):
-    session.run("poetry", "install")
-    session.run("black", "--check", ".")
-    session.run("flake8", ".")
+def tests(session):
+    # session.install("poetry")
+    # session.run("poetry", "install")
+    # session.install("-r", "requirements.txt")
+    # session.run("python", "-m", "coverage", "run", "-m", "pytest")
+    # session.run("coverage", "report")
+    session.notify('shared_venv', posargs=['tests'])
 
 
 @nox.session
-def typing(session: nox.Session):
-    session.run("poetry", "install")
-    session.run("mypy", "--config-file", "mypy.ini", ".")
+def lint(session):
+    # session.install("poetry")
+    # session.run("poetry", "install")
+    # session.run("black", "--check", ".")
+    # session.run("flake8", ".")
+    session.notify('shared_venv', posargs=['lint'])
 
 
 @nox.session
-def safety(session: nox.Session):
+def typing(session):
+    # session.install("poetry")
+    # session.run("poetry", "install")
+    # session.run("mypy", "--config-file", "mypy.ini", ".")
+    session.notify('shared_venv', posargs=['typing'])
+
+
+@nox.session
+def safety(session):
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
