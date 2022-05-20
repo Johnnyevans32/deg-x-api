fae8fa076da0fe92b02d35bbc43aee86881ed268
johnny evans <evanseburu32@gmail.com>
2022-05-21 00:18:20 +0100

    circle ci

--- noxfile.py
+++ noxfile.py
@@ -3,56 +3,55 @@ import tempfile
 import nox
 
 
+# @nox.session(python=["3.10"])
+# def shared_venv(session: nox.Session):
+#     session.install("poetry")
+#     session.run("poetry", "install")
+#     session.install("-r", "requirements.txt")
+
+#     if session.posargs:
+#         match session.posargs[0]:
+#             case 'tests':
+#                 session.run("python", "-m", "coverage", "run", "-m", "pytest")
+#                 session.run("coverage", "report")
+#             case 'lint':
+#                 session.run("black", "--target-version", "py310")
+#                 session.run("flake8", ".")
+#             case 'typing':
+#                 session.run("mypy", "--config-file", "mypy.ini", ".", "--show-traceback")
+#             case _:
+#                 pass
+
+
 @nox.session(python=["3.10"])
-def shared_venv(session):
+def test_build(session: nox.Session):
     session.install("poetry")
     session.run("poetry", "install")
     session.install("-r", "requirements.txt")
-
-    if session.posargs:
-        match session.posargs[0]:
-            case 'tests':
-                session.run("python", "-m", "coverage", "run", "-m", "pytest")
-                session.run("coverage", "report")
-            case 'lint':
-                print('jsjksksk')
-                session.run("black", "--target-version", "py310")
-                session.run("flake8", ".")
-            case 'typing':
-                session.run("mypy", "--config-file", "mypy.ini", ".")
-            case _:
-                pass
+    session.run("python", "-m", "coverage", "run", "-m", "pytest")
+    session.run("coverage", "report")
+    # session.notify('shared_venv', posargs=['tests'])
 
 
 @nox.session
-def tests(session):
-    # session.install("poetry")
-    # session.run("poetry", "install")
-    # session.install("-r", "requirements.txt")
-    # session.run("python", "-m", "coverage", "run", "-m", "pytest")
-    # session.run("coverage", "report")
-    session.notify('shared_venv', posargs=['tests'])
+def lint_build(session: nox.Session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("black", "--check", ".")
+    session.run("flake8", ".")
+    # session.notify('shared_venv', posargs=['lint'])
 
 
 @nox.session
-def lint(session):
-    # session.install("poetry")
-    # session.run("poetry", "install")
-    # session.run("black", "--check", ".")
-    # session.run("flake8", ".")
-    session.notify('shared_venv', posargs=['lint'])
+def typing_build(session: nox.Session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("mypy", "--config-file", "mypy.ini", ".")
+    # session.notify('shared_venv', posargs=['typing'])
 
 
 @nox.session
-def typing(session):
-    # session.install("poetry")
-    # session.run("poetry", "install")
-    # session.run("mypy", "--config-file", "mypy.ini", ".")
-    session.notify('shared_venv', posargs=['typing'])
-
-
-@nox.session
-def safety(session):
+def safety_build(session: nox.Session):
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
